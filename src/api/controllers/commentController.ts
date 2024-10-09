5d490516a9362d773dc58c9ea48ff5ddf38cac7e
Tatyanac94 <ms.cuttino94@gmail.com>
2024-10-08 20:41:44 -0400

    added file for CRUD operations for Posts

--- src/api/controllers/commentController.ts
+++ src/api/controllers/commentController.ts
@@ -2,11 +2,12 @@ import { Request, Response } from 'express';
 import { supabase } from '../../config/supabaseClient';
 
 export const createComment = async (req: Request, res: Response): Promise<Response> => {
-  const { postid, user_id, content } = req.body;
+  const { postid, content, name } = req.body;
+  const username = name?.trim() || 'anonymous';
 
   const { data, error } = await supabase
     .from('supportcomment')
-    .insert([{ postid, user_id, content }]);
+    .insert([{ postid, username, content }]);
 
   if (error) return res.status(400).json({ error: error.message });
   return res.status(201).json(data);
