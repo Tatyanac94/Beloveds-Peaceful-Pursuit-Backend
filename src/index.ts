a1509e8f1e7fc70087f8fd8f817fbb25fb6ec588
Tatyanac94 <ms.cuttino94@gmail.com>
2024-10-10 16:03:08 -0400

    updated main entry point

--- src/index.ts
+++ src/index.ts
@@ -8,13 +8,10 @@ import postLikeRoutes from './api/routes/postLikeRoutes';
 import commentLikeRoutes from './api/routes/commentLikeRoutes';
 import { errorHandler } from './api/middleware/errorHandler';
 
-// Load environment variables from .env file
 dotenv.config();
 
-// Create an instance of the express application
 const app = express();
 
-// Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -22,17 +19,14 @@ app.get('/', (req, res) => {
   res.send('Welcome to the Mental Health Support Platform API!');
 });
 
-// Define routes
 app.use('/forums', forumRoutes);
 app.use('/posts', postRoutes);
 app.use('/comments', commentRoutes);
 app.use('/post-likes', postLikeRoutes);
 app.use('/comment-likes', commentLikeRoutes);
 
-// Error handling middleware
 app.use(errorHandler);
 
-// Start the server
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
